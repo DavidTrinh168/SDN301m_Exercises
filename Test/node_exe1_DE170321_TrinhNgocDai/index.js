5d0accfb6ebb192a99648923ec954172cd48bb49
Trinh Ngoc Dai <daingoctrinh72711@gmail.com>
2024-05-14 23:26:52 +0700

    Week 2 test 1, after Exercise 5

---
+++ Test/node_exe1_DE170321_TrinhNgocDai/index.js
@@ -0,0 +1,28 @@
+const express = require('express'), http = require('http');
+const morgan = require('morgan');
+const hostname = 'localhost';
+const port = 3000;
+const app = express();
+const bodyParser = require('body-parser');
+const gerneRouter = require('./routes/genresRouter');
+const authorRouter = require('./routes/authorRouter');
+const bookRouter = require('./routes/booksRouter');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/book', bookRouter);
+app.use('/genre', gerneRouter);
+app.use('/author', authorRouter);
+
+app.use((req, res, next) => {
+    // console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
