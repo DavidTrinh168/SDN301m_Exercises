05b2b838903aefa4b9c2374ed58b6dfcb1ca1d3f
Trinh Ngoc Dai <daingoctrinh72711@gmail.com>
2024-06-05 14:42:48 +0700

    Express REST API with MongoDB and Mongoose Part 2

---
+++ Exercise14-conFusionServer/app.js
@@ -0,0 +1,63 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const Dishes = require("./models/dishes");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const gerneRouter = require("./routes/genresRouter");
+const authorRouter = require("./routes/authorRouter");
+const bookRouter = require("./routes/booksRouter");
+const dishRouter = require("./routes/dishRouter");
+
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
+
+connect.then(
+  (db) => {
+    console.log("Connected correctly to server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/book", bookRouter);
+app.use("/genre", gerneRouter);
+app.use("/author", authorRouter);
+app.use("/dishes", dishRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
